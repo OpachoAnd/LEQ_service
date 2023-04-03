07d9b3f521cff53e72db221f0aa197e9cbe22cf3
OpachoAnd <andrej.pchelin@gmail.com>
2023-04-03 22:05:35 +0500

    added rabbit listener and docker-compose

--- main.py
+++ main.py
@@ -11,7 +11,31 @@ from handlers.transfer_weights_head import transfer_weights
 from handlers.extract_audio import extract
 from handlers.transforms_concat import concatenation
 
+import pika
+
 HOME_DIRECTORY = str(Path(__file__).absolute().parent)
+RMQ_HOST = 'mq'
+RMQ_PORT = 5672
+RMQ_USER = 'guest'
+RMQ_PASSWORD = 'guest'
+
+
+def handling_video_callback(ch, method, properties, body):
+    video_id = body
+    path_config_head = f'{HOME_DIRECTORY}/cloud/{video_id}/HeadNeRF_config.txt'
+    path_config_torso = f'{HOME_DIRECTORY}/cloud{video_id}/TorsoNeRF_config.txt'
+    path_weight_head = f'{HOME_DIRECTORY}/cloud/{video_id}/logs/{video_id}_head'
+    path_weight_torso = f'{HOME_DIRECTORY}/cloud/{video_id}/logs/{video_id}_com'
+    path_config_inference = f'{HOME_DIRECTORY}/cloud/{video_id}/TorsoNeRFTest_config.txt'
+    path_audio = f'{HOME_DIRECTORY}/cloud/{video_id}/aud.npy'
+
+    preprocessing_processes(video_id)
+    train_head(path_config_head)
+    transfer_weights(path_weight_head, path_weight_torso)
+    train_torso(path_config_torso)  # был path_config_head
+    extract(HOME_DIRECTORY, video_id)
+    concatenation(HOME_DIRECTORY, video_id)
+    inference(path_config_inference, path_audio)
 
 
 if __name__ == "__main__":
@@ -19,18 +43,17 @@ if __name__ == "__main__":
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
     mp.set_start_method(method='spawn', force=True)
 
-    path_config_head = f'{HOME_DIRECTORY}/cloud/{video_id}/HeadNeRF_config.txt'
-    path_config_torso = f'{HOME_DIRECTORY}/cloud{video_id}/TorsoNeRF_config.txt'
-    path_weight_head = f'{HOME_DIRECTORY}/cloud/{video_id}/logs/{video_id}_head'
-    path_weight_torso = f'{HOME_DIRECTORY}/cloud/{video_id}/logs/{video_id}_com'
-    path_config_inference = f'{HOME_DIRECTORY}/cloud/{video_id}/TorsoNeRFTest_config.txt'
-    path_audio = f'{HOME_DIRECTORY}/cloud/{video_id}/aud.npy'
-    # preprocessing_processes(video_id)
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=RMQ_HOST,
+                                                                       port=RMQ_PORT))
 
-    # train_head(path_config_head)
-    # transfer_weights(path_weight_head, path_weight_torso)
-    # train_torso(path_config_head)
+        channel = connection.channel()
+        channel.queue_declare(queue='ad_nerf')
+        channel.basic_consume(queue='ad_nerf',
+                              on_message_callback=handling_video_callback,
+                              auto_ack=True)
 
-    # extract(HOME_DIRECTORY, video_id)
-    concatenation(HOME_DIRECTORY, video_id)
-    inference(path_config_inference, path_audio)
+        print('ok_connection', flush=True)
+
+    except:
+        print('error_connection', flush=True)
