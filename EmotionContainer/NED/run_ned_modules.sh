451c76b1e656d4d3118474e3d587462dbd4aa0bc
KadMary <kadochnikovamasha@mail.ru>
2023-04-09 10:58:36 +0500

    add changes to dockerfile

--- EmotionContainer/NED/run_ned_modules.sh
+++ EmotionContainer/NED/run_ned_modules.sh
@@ -1,6 +1,8 @@
-emotions=$1
+trg_emotions=$@
+
+echo "Run video with emotions: $trg_emotions"
 
 bash ./preprocess.sh actor
-python manipulator/test.py --celeb actor --checkpoints_dir ./manipulator_checkpoints --trg_emotions $emotions --exp_name result
+python manipulator/test.py --celeb actor --checkpoints_dir ./manipulator_checkpoints --trg_emotions $trg_emotions --exp_name result
 bash ./postprocess.sh actor result checkpoints_actor
 python postprocessing/images2video.py --imgs_path actor/result/full_frames --out_path new_actor.mp4 --audio actor/videos/actor.mp4
--- EmotionContainer/emotion.sh
+++ EmotionContainer/emotion.sh
@@ -1,12 +1,35 @@
 celeb=$1
-emotions=$(cat emotions.txt)
+trg_emotions=$(cat /home/malinux/NED/emotions.txt)
 
 
+echo "Copy weights..."
+rm -rf ./NED/checkpoints_actor
+mkdir ./NED/checkpoints_actor
+if [ "$celeb" = "pacino" ]
+then
+    cp -a /home/malinux/NED/actors/checkpoints_pacino/. ./NED/checkpoints_actor/
+elif [ "$celeb" = "roberts" ]
+then
+    cp -a /home/malinux/NED/actors/checkpoints_roberts/. ./NED/checkpoints_actor/
+elif [ "$celeb" = "tarantino" ]
+then
+    cp -a /home/malinux/NED/actors/checkpoints_tarantino/. ./NED/checkpoints_actor/
+else
+    echo "Invalid mode given"
+fi
+cp -a /home/malinux/NED/weights/DECA/data/. ./NED/DECA/data/
+cp -a /home/malinux/NED/weights/manipulator_checkpoints/. ./NED/manipulator_checkpoints/
+cp -a /home/malinux/NED/weights/preprocessing/segmentation/. ./NED/preprocessing/segmentation/
+
+echo "Copy video..."
+rm -rf ./NED/actor
+mkdir ./NED/actor
+cp -a /home/malinux/NED/video/. ./NED/actor/videos/
 
 echo "Build Docker"
-sudo docker build . -t ned -f NED/Dockerfile
+docker build . -t ned -f NED/Dockerfile 
 
 echo "Run Docker"
-sudo docker run --name ned_container -t ned $emotions
-
-echo "Ready?"
+docker run --name ned_container --gpus all -t ned $trg_emotions
+docker cp ned_container:/app/new_actor.mp4  /home/malinux/NED
+echo "Ready!"
